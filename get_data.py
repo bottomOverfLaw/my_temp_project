a9161003ef955ff70f17d6b0c2868a77772d1c32
bottomOverfLaw <laura.alb02@gmail.com>
2026-06-30 11:59:57 +1000

    edit: modified the code to use real data from the board using pyserial

--- get_data.py
+++ get_data.py
@@ -1,11 +1,12 @@
 """
 Use CDM not powershell to run this
-command to use: py -u fake_sensor.py | py get data
+command to use: py -u fake_sensor.py | py get_data.py
 
 """
 
 import sqlite3
-import sys #sys.stdins for now then when I'll have the real data will use pyserial
+#sys.stdins for now then when I'll have the real data will use pyserial
+import serial
 from datetime import datetime
 
 dataBase = "tempdata.db"
@@ -31,17 +32,30 @@ try:
             print("Table create succesfully")
 
             cursor = conn.cursor()
-            for line in sys.stdin:
-                if "TEMP:" in line:
-                    data = line.split("TEMP:")[1]
+
+            #open the port to read the data sent from the ADC with UART
+            serPort = serial.Serial(
+                port = 'COM5', baudrate = 115200, bytesize = 8, timeout = 2
+            )
+            
+            while True:
+
+                #read the line and decode them
+                line = serPort.readline()
+                decode_line = line.decode('utf-8').strip()
+
+                #check the data read and in case match the format start splitting it, keeping only the number and cast from string back to float
+                if "TEMP:" in decode_line:
+                    data = decode_line.split("TEMP:")[1]
                     temp = float(data)
                     
                     timest = datetime.now().isoformat()
                     
+                    #insert the data and the datetime in the db
                     cursor.execute("INSERT INTO Temperature (temperature, timestamp) VALUES (?, ?)", (temp, timest))
 
                     conn.commit()
-                    #print("Insert ok")
+
         except sqlite3.OperationalError as e:
             print("Failed to create tables:", e)
         
