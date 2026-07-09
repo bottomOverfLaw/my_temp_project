e6ca9909acf92737456c7a11025a0060c56e974a
bottomOverfLaw <laura.alb02@gmail.com>
2026-07-09 18:05:03 +1000

    comment on how what to use to make it work

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+#Before F5 this file open CMD and start get_data.py after making the board work
+#Go on the project file directory and use this two commands:
+#Then on a new cmd start py -m uvicorn server:app --port 8001 --reload 
+#New cmd py -m uvicorn api:app --port 8000 --reload
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
