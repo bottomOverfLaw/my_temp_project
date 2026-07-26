0ab06e2a9be711c2698e904e178c27f472cfcc11
bottomOverfLaw <laura.alb02@gmail.com>
2026-07-26 13:59:52 +1000

    Add: __main__ entry point
    Allows running the script directly without needing CLI commands

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #Before F5 this file open CMD and start get_data.py after making the board work
 #Go on the project file directory and use this two commands:
-#Then on a new cmd start py -m uvicorn server:app --port 8001 --reload 
+#Then on a new cmd start py -m uvicorn server:app --port 8001 --reload -> no more needed since I can start directly from F5 here
 #New cmd py -m uvicorn api:app --port 8000 --reload
 
 from fastapi import FastAPI
@@ -10,3 +10,7 @@ app = FastAPI()
 
 # Mounts the local "dashboard" folder to the "/dashboard" URL path
 app.mount("/dashboard", StaticFiles(directory="dashboard", html=True), name="dashboard")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8001)
